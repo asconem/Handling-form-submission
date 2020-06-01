495d355ffc14792075fc3d1d62f46255fef56807
asconem <asconem@gmail.com>
2020-06-01 19:55:27 -0400

    Adding testing

--- src/test/java/com/example/handlingformsubmission/HandlingFormSubmissionApplicationTests.java
+++ src/test/java/com/example/handlingformsubmission/HandlingFormSubmissionApplicationTests.java
@@ -1,13 +1,35 @@
 package com.example.handlingformsubmission;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
-@SpringBootTest
-class HandlingFormSubmissionApplicationTests {
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(GreetingController.class)
+@TestPropertySource(properties = "logging.level.org.springframework.web=DEBUG")
+public class HandlingFormSubmissionApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
-	void contextLoads() {
+	public void rendersForm() throws Exception {
+		mockMvc.perform(get("/greeting"))
+				.andExpect(content().string(containsString("Form")));
+	}
+
+	@Test
+	public void submitsForm() throws Exception {
+		mockMvc.perform(post("/greeting").param("id", "12345").param("content", "Hello"))
+				.andExpect(content().string(containsString("Result")))
+				.andExpect(content().string(containsString("id: 12345")));
 	}
 
 }
